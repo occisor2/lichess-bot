45828714bdf93f585cd201ba2d7af02376862f5e
Russell Smith <russell.smith7502@gmail.com>
2025-04-23 18:18:02 -0500

    Add minimax and evalulation stubs

--- engines/badbot/badbot.py
+++ engines/badbot/badbot.py
@@ -1,12 +1,21 @@
 from lib.engine_wrapper import MinimalEngine
 import lib.lichess_types as lichess_types
+from typing import override
 import chess
 import random
 
 
 class BadBot(MinimalEngine):
+    @override
     def search(self, board: chess.Board, time_limit: chess.engine.Limit,
                ponder: bool, draw_offered: bool,
                root_moves: lichess_types.MOVE) -> chess.engine.PlayResult:
         move = random.choice(list(board.legal_moves))
         return chess.engine.PlayResult(move, None)
+
+    def evaluate(board: chess.Board) -> float:
+        pass
+
+    def minimax(board: chess.Board, depth: int, alpha: float, beta: float,
+                maxing_player) -> float:
+        pass
